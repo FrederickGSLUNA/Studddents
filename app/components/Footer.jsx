9445bc547b6e14c626046fac37633b5f60570c70
Frederick Guzman <frederickguzman2236@gmail.com>
2023-05-09 23:33:41 -0600

    Fix footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -25,8 +25,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr class='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
-        <span class='block text-sm text-gray-500 sm:text-center dark:text-gray-400'>© 2023 <a href='/' class='hover:underline'>Studddents</a>. All Rights Reserved.</span>
+        <hr class='my-6 border-gray-200 sm:mx-auto lg:my-8' />
+        <span class='block text-sm text-gray-500 sm:text-center'>© 2023 <a href='/' class='hover:underline'>Studddents</a>. All Rights Reserved.</span>
       </div>
     </footer>
 
